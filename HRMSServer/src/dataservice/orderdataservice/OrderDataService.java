de106875a7ea960938d663fcbd1d357f02d37c93
AlintaLu <2008luyiqing@163.com>
2016-10-14 21:02:17 +0800

    ～

--- HRMSServer/src/dataservice/orderdataservice/OrderDataService.java
+++ HRMSServer/src/dataservice/orderdataservice/OrderDataService.java
@@ -7,23 +7,60 @@ import po.OrderPO;
 import po.UserType;
 
 public interface OrderDataService {
-	// 在数据库中增加一个订单记录
+	/**
+	 * 在数据库中增加一个订单记录
+	 * @param order
+	 * @return  添加成功（true）或失败（false）的布尔值
+	 */
 	public boolean add(OrderPO order);
-	// 在数据库中保存一个订单记录
+	
+	/**
+	 * 在数据库中保存一个订单记录
+	 * @param order
+	 * @return 添加成功（true）或失败（false）的布尔值
+	 */
 	public boolean save(OrderPO order);
-	// 将数据库中将订单置为取消状态
+	
+	/**
+	 * 将数据库中将订单置为取消状态
+	 * @param orderId
+	 * @return 置为取消状态成功（true）或失败（false）的布尔值
+	 */
 	public boolean cancel(String orderId);
-	// 查找某用户的具体订单
+	
+	/**
+	 * 查找某用户的具体订单
+	 * @param userType
+	 * @param orderId
+	 * @return
+	 */
 	public OrderPO findSpecificClientOrder(UserType userType, String orderId);
 	
-	// 查找某客户的所有订单
+	/**
+	 * 查找某客户的所有订单
+	 * @param userId
+	 * @return
+	 */
 	public List<OrderPO> findUserOrderList(String userId);
-	// 查找数据库中某客户的某个种类（正常未执行、正常已执行、取消、异常）的订单
+	
+	/**
+	 *  查找数据库中某客户的某个种类（正常未执行、正常已执行、取消、异常）的订单
+	 * @param clientId
+	 * @return 订单列表
+	 */
 	public List<OrderPO> findClientTypeOrder (String clientId);
 	
-	// 查找数据库中某天的异常订单
+	/**
+	 * 查找数据库中某天的异常订单
+	 * @param date
+	 * @return 异常订单列表
+	 */
 	public List<OrderPO> findAbnormalOrderList(Date date);
 	
-	// 根据时间，在数据库中将超时订单置为异常状态
+	/**
+	 * 根据时间，在数据库中将超时订单置为异常状态
+	 * @param orderId
+	 */
 	public void checkTime (String orderId);
+	
 }
--- HRMSServer/src/dataservice/sitemanagerdataservice/SitemanagerDataService.java
+++ HRMSServer/src/dataservice/sitemanagerdataservice/SitemanagerDataService.java
@@ -2,7 +2,9 @@ package dataservice.sitemanagerdataservice;
 
 import po.SitemanagerPO;
 public interface SitemanagerDataService {
-	
-	// 从数据库中得到网站管理人员账户信息
+	/**
+	 * 从数据库中得到网站管理人员账户信息
+	 * @return 网站管理人员PO
+	 */
 	public SitemanagerPO getAccount();
 }
