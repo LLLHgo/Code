bbfd4814810849411cf8b9eb4a785b1e9119f91c
sweetStreet <942290857@qq.com>
2016-10-14 17:25:52 +0800

    vo

---
+++ HRMSClient/src/businesslogicservice/HotelStaffblservice.java
@@ -0,0 +1,26 @@
+package businesslogicservice;
+
+import java.util.List;
+import vo.HotelOrderVO;
+import vo.HotelStrategyVO;
+import vo.HotelinfoVO;
+import vo.HotelstaffBasicinfoVO;
+import vo.HotelstaffVO;
+import vo.ResultMessage;
+import vo.RoominfoVO;
+
+public interface HotelStaffblservice {
+	
+	public HotelstaffBasicinfoVO getBasicinfo(String hotelID);
+	public ResultMessage setPassword(String password);
+	public ResultMessage updatehotelStrategy(HotelStrategyVO vo);
+	public HotelStrategyVO gethotelStrategy(String hotelID);
+	public HotelinfoVO gethotelinfoVO(HotelinfoVO vo);
+	public RoominfoVO getroominfo(String roomID);
+	public ResultMessage updateroominfo(HotelinfoVO vo);
+	public List<HotelOrderVO> gethotelOrderList(String hotelID);
+	public ResultMessage updateOrderState(String orderID,int state);
+	public boolean saveSitemanagerUpdate(HotelstaffVO vo);
+	public HotelstaffVO returnSitemanagerAccount(String hotelID);
+	public boolean checkAccount(String hotelID,String password);
+}
