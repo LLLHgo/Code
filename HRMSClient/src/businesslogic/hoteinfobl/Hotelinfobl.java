ef9911282ae887ca9399c39af6c1f6c8a0062a4e
belikout <1243149017@qq.com>
2016-10-14 23:23:50 +0800

    ffff

--- HRMSClient/src/businesslogic/hoteinfobl/Hotelinfobl.java
+++ HRMSClient/src/businesslogic/hoteinfobl/Hotelinfobl.java
@@ -1,9 +1,6 @@
 package businesslogic.hoteinfobl;
 
-<<<<<<< HEAD:HRMSClient/src/businesslogic/hoteinfobl/Hotelinfobl.java
 
-=======
->>>>>>> origin/master:HRMSClient/src/businesslogic/hoteinfobl/HotelInfobl.java
 public class Hotelinfobl {
 
 }
