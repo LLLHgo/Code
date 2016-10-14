1f8547dbc663737aefa072d2569c56d63d5b6d0a
WinwindH <huangpiao@huangpiaodeMacBook-Pro.local>
2016-10-14 21:08:12 +0800

    .

--- HRMSClient/src/vo/strategyVO/MarketingStrategyVO.java
+++ HRMSClient/src/vo/strategyVO/MarketingStrategyVO.java
@@ -8,18 +8,106 @@ import Enum.*;
 import vo.*;
 
 public class MarketingStrategyVO {
+	//网站营销人员制定的策略的种类，有enum：marketingStrategy{DOUBLE11,VIPSPECIAL,CRATEDE;}
     private marketingStrategy type;
+    //活动开始时间
     private Date startTime;
+    //活动结束时间
     private Date endTime;
+    //活动折扣
     private double discount;
+    //参加VIP特定专属商圈折扣的商圈
     private ArrayList<String> businessArea;
+    //参加活动的酒店
     private ArrayList<HotelinfoVO> hotels;
+    //活动海报
     private Image poster;
+    //活动海报显示在客户界面的位置
     private String position;
+    //享受折扣的最低消费金额
     private int minSum;
+    //享受折扣的最少房间数
     private int minRooms;
-    private LevelVO Levels;
+    //参加活动的最低会员等级
+    private LevelVO levels;
+    //参加活动的会员种类
     private ArrayList<VIPType> vipKinds;
+
+    /**
+     * 营销人员制定双十一促销策略
+     *
+     * @param type  网站营销人员制定的策略的种类，这里为DOUBLE11
+     * @param startTime 活动开始时间
+     * @param endTime 活动结束时间
+     * @param discount 活动折扣
+     * @param hotels 参加活动的酒店
+     * @param poster 活动海报
+     * @param position 活动海报显示在客户界面的位置
+     */
+    public MarketingStrategyVO(marketingStrategy type,Date startTime,Date endTime,double discount,ArrayList<HotelinfoVO> hotels,
+    		Image poster,String position){
+    	this.type=type;
+    	this.startTime=startTime;
+    	this.endTime=endTime;
+    	this.discount=discount;
+    	this.hotels=hotels;
+    	this.poster=poster;
+    	this.position=position;
+    }
+
+    /**
+     * 网站营销人员制定的VIP特定商圈折扣
+     *
+     * @param type  网站营销人员制定的策略的种类，这里为VIPSPECIAL
+     * @param startTime
+     * @param endTime
+     * @param discount
+     * @param poster
+     * @param businessArea
+     * @param position
+     */
+    public MarketingStrategyVO(marketingStrategy type,Date startTime,Date endTime,double discount,Image poster,
+    		ArrayList<String> businessArea,String position){
+    	this.type=type;
+    	this.startTime=startTime;
+    	this.endTime=endTime;
+    	this.discount=discount;
+    	this.businessArea=businessArea;
+    	this.poster=poster;
+    	this.position=position;
+    }
+
+    /**
+     *
+     * @param type    网站营销人员制定的策略的种类，这里为CREATED
+     * @param startTime
+     * @param endTime
+     * @param discount
+     * @param hotels
+     * @param businessArea
+     * @param poster
+     * @param position
+     * @param minSum
+     * @param minRooms
+     * @param levels
+     * @param vipKinds
+     */
+    public MarketingStrategyVO(marketingStrategy type,Date startTime,Date endTime,double discount,ArrayList<HotelinfoVO> hotels,
+    		ArrayList<String> businessArea,Image poster,String position,int minSum,int minRooms,LevelVO levels,
+    		ArrayList<VIPType> vipKinds){
+    	this.type=type;
+    	this.startTime=startTime;
+    	this.endTime=endTime;
+    	this.discount=discount;
+    	this.hotels=hotels;
+    	this.businessArea=businessArea;
+    	this.poster=poster;
+    	this.position=position;
+    	this.minSum=minSum;
+    	this.minRooms=minRooms;
+    	this.levels=levels;
+    	this.vipKinds=vipKinds;
+    }
 	public marketingStrategy getType() {
 		return type;
 	}
@@ -81,10 +169,10 @@ public class MarketingStrategyVO {
 		this.minRooms = minRooms;
 	}
 	public LevelVO getLevels() {
-		return Levels;
+		return levels;
 	}
 	public void setLevels(LevelVO levels) {
-		Levels = levels;
+		this.levels = levels;
 	}
 	public ArrayList<VIPType> getVipKinds() {
 		return vipKinds;
