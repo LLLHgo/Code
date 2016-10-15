5901c4cdff8b6494514f5538f1c3c688f98a9bc8
sweetStreet <942290857@qq.com>
2016-10-15 21:43:46 +0800

    ..

--- HRMSServer/src/dataservice/hotelstaffdataservice/HotelstaffDataService_Driver.java
+++ HRMSServer/src/dataservice/hotelstaffdataservice/HotelstaffDataService_Driver.java
@@ -1,8 +1,15 @@
 package dataservice.hotelstaffdataservice;
 
+import java.rmi.RemoteException;
+
+import po.HotelstaffPO;
 
 public class HotelstaffDataService_Driver {
-	public void drive(HotelstaffDataService hotelstaffDataService){
-		
+	public void drive(HotelstaffDataService hotelstaffDataService) throws RemoteException{
+		hotelstaffDataService.init();
+		hotelstaffDataService.checkAccount(new HotelstaffPO());
+		hotelstaffDataService.findBasicInfo("H00000000");
+		hotelstaffDataService.updatePassword(new HotelstaffPO());
+		hotelstaffDataService.finish();
 	}
 }
