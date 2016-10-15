4ed772d4307f3abe1e34feb402f4233027a3b6c4
WinwindH <huangpiao@huangpiaodeMacBook-Pro.local>
2016-10-15 20:51:51 +0800

    ，

--- HRMSServer/src/dataservice/strategydataservice/StrategyDataService.java
+++ HRMSServer/src/dataservice/strategydataservice/StrategyDataService.java
@@ -3,6 +3,7 @@ package dataservice.strategydataservice;
 import java.util.List;
 
 import Enum.VIPType;
+import po.HotelStrategyPO;
 import po.MarketingStrategyPO;
 import po.PrivilegePO;
 
@@ -28,5 +29,38 @@ public interface StrategyDataService {
      */
     public boolean deletePrivilege(PrivilegePO po);
 
+    /**
+     * 增加网站营销人员促销策略
+     * @param po
+     * @return 是否增加成功
+     */
     public boolean addMarketingStrategy(MarketingStrategyPO po);
+
+    /**
+     * 返回网站营销人员指定的额策略列表
+     * @param id
+     * @return 返回策略列表
+     */
+    public List<MarketingStrategyPO> getMarketingStrategy (String id);
+
+    /**
+     * 删除网站营销人员制定的策略
+     * @param po
+     * @return 返回是否删除成功
+     */
+    public boolean deleteMarketingStrategy (MarketingStrategyPO po);
+
+    /**
+     * 根据hotelID返回该酒店制定的策略
+     * @param hotelID
+     * @return 返回策略列表
+     */
+    public List<HotelStrategyPO> getHotelStrategy(String hotelID);
+
+    /**
+     * 更新酒店策略
+     * @param po
+     * @return 返回是否更新成功
+     */
+    public boolean updateHotelStrategy(HotelStrategyPO po);
 }
