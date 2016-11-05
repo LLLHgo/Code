23ce36e935226b0c4a4b3dfaeb576454fbf87be8
belikout <1243149017@qq.com>
2016-11-05 21:43:43 +0800

    jjk

--- HRMSClient/src/main/java/presentation/client/controller/ProcessClientViewController.java
+++ HRMSClient/src/main/java/presentation/client/controller/ProcessClientViewController.java
@@ -2,6 +2,7 @@ package presentation.client.controller;
 
 import java.io.File;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import Enum.OrderType;
@@ -15,88 +16,157 @@ import vo.orderVO.OrderVO;
 
 public class ProcessClientViewController implements ProcessClientViewControllerService {
 
-	@Override
-	public ClientVO getclient(String clientID) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 *通过客户ID得到客户个人信息
+	 * @param clientID
+	 * @return 客户的个人信息
+	 */
+	public ClientVO getclient (String clientID){
+		return new ClientVO();
 	}
 
-	@Override
-	public boolean createClient(ClientVO vo) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 *增加新客户
+	 * @param vo
+	 * @return 增加新用户成功与否
+	 */
+	public boolean createClient(ClientVO vo){
+		if(vo.getID() != null)
+			return false;
+		else
+			return true;
 	}
-
-	@Override
-	public boolean setCredit(String clientID, int recharge) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 *设置（修改）某一客户信用值
+	 * @param clientID
+	 * @param recharge
+	 * @return 修改信用值成功与否
+	 */
+	public boolean setCredit(String clientID,int recharge){
+		if(recharge==0)
+			return false;
+		else
+			return true;
 	}
-
-	@Override
-	public boolean setAllClientLevel(LevelVO vo) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * 根据新的会员等级制度修改所有客户的会员等级
+	 * @param vo
+	 * @return 修改会员等级成功与否
+	 */
+	public boolean setAllClientLevel(LevelVO vo){
+		if(vo!=null)
+			return true;
+		else
+			return false;
 	}
-
-	@Override
-	public List<HotelinfoVO> getSearchHotel(ClientRequirementVO vo) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * 通过完整正确的酒店关键词得到相关酒店列表
+	 * @param vo
+	 * @return符合关键词的酒店列表
+	 */
+	public List<HotelinfoVO> getSearchHotel(ClientRequirementVO vo){
+		List<HotelinfoVO> hotel=new ArrayList<HotelinfoVO>();
+		return hotel;
 	}
-
-	@Override
-	public List<OrderVO> getSearchDateOrder(String clientID, Date date) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 *得到具体日期的客户订单
+	 * @param clientID
+	 * @param date
+	 * @return 该日期的客户订单
+	 */
+	public List<OrderVO> getSearchDateOrder(String clientID,Date date){
+		List<OrderVO> order=new ArrayList<OrderVO>();
+		return order;
 	}
-
-	@Override
-	public List<OrderVO> getSearchHotelOrder(String clientID, String hotelID) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 *得到具体酒店的客户订单
+	 * @param clientID
+	 * @param hotelID
+	 * @return 该酒店的客户订单
+	 */
+	public List<OrderVO> getSearchHotelOrder( String clientID,String hotelID){
+		List<OrderVO> order=new ArrayList<OrderVO>();
+		return order;
 	}
-
-	@Override
-	public ResultMessage updateInfo(ClientVO vo) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 *修改客户基本信息
+	 * @param vo
+	 * @return 修改个人信息成功与否提示信息
+	 */
+	public  ResultMessage updateInfo(ClientVO vo){
+		if(vo!=null)
+			return ResultMessage.SUCCESS;
+		else
+			return ResultMessage.FAIL;
 	}
-
+	/**
+	 *创建订单
+	 * @param orderID
+	 * @return 创建成功与否
+	 */
+	public boolean createOrder(OrderVO vo){
+		if(vo!=null)
+			return true;
+		else
+			return false;
+	}
+	/**
+	 *撤销订单
+	 * @param orderID
+	 * @return 撤销成功与否
+	 */
+	public boolean repealOrder(String orderID){
+		if(orderID!=null)
+			return true;
+		else
+			return false;
+	}
+	/**
+	 *得到客户的所有订单
+	 * @param clientID
+	 * @param order_type
+	 * @return 所有订单列表
+	 */
+	public List<OrderVO> getAllOrderList(String clientID,OrderType order_type){
+		List<OrderVO> order=new ArrayList<OrderVO>();
+		return order;
+	}
+	/**
+	 *得到客户某一类型的订单
+	 * @param clientID
+	 * @param order_type
+	 * @return 类型订单列表
+	 */
+	public List<OrderVO> getOrderList(String clientID,OrderType order_type){
+		List<OrderVO> order=new ArrayList<OrderVO>();
+		return order;
+	}
+	/**
+	 *得到客户的信用记录
+	 * @param clientID
+	 * @return 信用记录
+	 */
+	public File getCreditRecord(String clientID){
+		if(clientID!=null)
+			return new File(clientID);
+		else
+			return null;
+	}
+	/**
+	 *得到客户的历史预订酒店列表
+	 * @param clientID
+	 * @return 历史预订酒店列表
+	 */
+	public List<HotelinfoVO> getHistoryList(String clientID){
+		List<HotelinfoVO> hotel=new ArrayList<HotelinfoVO>();
+		return hotel;
+	}
 	@Override
 	public boolean Order(OrderVO vo) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
-	public boolean repealOrder(String orderID) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
-	@Override
-	public List<OrderVO> getAllOrderList(String clientID, OrderType order_type) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<OrderVO> getOrderList(String clientID, OrderType order_type) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public File getCreditRecord(String clientID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<HotelinfoVO> getHistoryList(String clientID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
--- HRMSClient/src/test/java/Mock/MockClient.java
+++ HRMSClient/src/test/java/Mock/MockClient.java
@@ -2,8 +2,16 @@ package Mock;
 
 import java.io.File;
 import java.sql.Date;
-
+import java.util.ArrayList;
+import java.util.List;
+import Enum.OrderType;
+import Enum.ResultMessage;
 import Enum.VIPType;
+import vo.clientVO.ClientVO;
+import vo.hotelinfoVO.ClientRequirementVO;
+import vo.hotelinfoVO.HotelinfoVO;
+import vo.levelVO.LevelVO;
+import vo.orderVO.OrderVO;
 
 public class MockClient {
 	//用户编号
@@ -24,30 +32,166 @@ public class MockClient {
 		 String client_firm;
 	//用户信用记录
 		File creditRecord;
-		public MockClient(){
 
+		public MockClient(){
 		}
 		public MockClient(String id,String password){
 			this.client_id=id;
 			this.password=password;
 		}
-		public MockClient(String id,VIPType v,Date client_birth){
-			this.client_id=id;
-			this.vip_type=v;
-			this.client_birth=client_birth;
-		}
-		public MockClient(String id,VIPType v,String client_firm){
-			this.client_id=id;
-			this.vip_type=v;
-			this.client_firm=client_firm;
-		}
-		public MockClient(String id,String name,String tel){
-			this.client_id=id;
+		public MockClient(String name){
 			this.client_name=name;
-			this.client_tel=tel;
 		}
-		public MockClient(String id,File c){
-			this.client_id=id;
-			this.creditRecord=c;
+		@SuppressWarnings("deprecation")
+		Date day=new Date(1996,2,29);
+		File file=new File("c.txt");
+		ClientVO client=new ClientVO("C00000001","justfun","ErgouWang","13747474741",
+				 VIPType.ORDINARYVIP,2,day,"",file);
+		/**
+		 *通过客户ID得到客户个人信息
+		 * @param clientID
+		 * @return 客户的个人信息
+		 */
+		public ClientVO getclient (String clientID){
+			return client;
 		}
+
+		/**
+		 *增加新客户
+		 * @param vo
+		 * @return 增加新用户成功与否
+		 */
+		public boolean createClient(ClientVO vo){
+			if(vo.getID() != null)
+				return false;
+			else
+				return true;
+		}
+		/**
+		 *设置（修改）某一客户信用值
+		 * @param clientID
+		 * @param recharge
+		 * @return 修改信用值成功与否
+		 */
+		public boolean setCredit(String clientID,int recharge){
+			if(recharge==0)
+				return false;
+			else{
+				return true;
+			}
+		}
+		/**
+		 * 根据新的会员等级制度修改所有客户的会员等级
+		 * @param vo
+		 * @return 修改会员等级成功与否
+		 */
+		public boolean setAllClientLevel(LevelVO vo){
+			if(vo!=null)
+				return true;
+			else
+				return false;
+		}
+		/**
+		 * 通过完整正确的酒店关键词得到相关酒店列表
+		 * @param vo
+		 * @return符合关键词的酒店列表
+		 */
+		public List<HotelinfoVO> getSearchHotel(ClientRequirementVO vo){
+			List<HotelinfoVO> hotel=new ArrayList<HotelinfoVO>();
+			return hotel;
+		}
+		/**
+		 *得到具体日期的客户订单
+		 * @param clientID
+		 * @param date
+		 * @return 该日期的客户订单
+		 */
+		public List<OrderVO> getSearchDateOrder(String clientID,Date date){
+			List<OrderVO> order=new ArrayList<OrderVO>();
+			return order;
+		}
+		/**
+		 *得到具体酒店的客户订单
+		 * @param clientID
+		 * @param hotelID
+		 * @return 该酒店的客户订单
+		 */
+		public List<OrderVO> getSearchHotelOrder( String clientID,String hotelID){
+			List<OrderVO> order=new ArrayList<OrderVO>();
+			return order;
+		}
+		/**
+		 *修改客户基本信息
+		 * @param vo
+		 * @return 修改个人信息成功与否提示信息
+		 */
+		public  ResultMessage updateInfo(ClientVO vo){
+			if(vo!=null)
+				return ResultMessage.SUCCESS;
+			else
+				return ResultMessage.FAIL;
+		}
+		/**
+		 *创建订单
+		 * @param orderID
+		 * @return 创建成功与否
+		 */
+		public boolean createOrder(OrderVO vo){
+			if(vo!=null)
+				return true;
+			else
+				return false;
+		}
+		/**
+		 *撤销订单
+		 * @param orderID
+		 * @return 撤销成功与否
+		 */
+		public boolean repealOrder(String orderID){
+			if(orderID!=null)
+				return true;
+			else
+				return false;
+		}
+		/**
+		 *得到客户的所有订单
+		 * @param clientID
+		 * @param order_type
+		 * @return 所有订单列表
+		 */
+		public List<OrderVO> getAllOrderList(String clientID,OrderType order_type){
+			List<OrderVO> order=new ArrayList<OrderVO>();
+			return order;
+		}
+		/**
+		 *得到客户某一类型的订单
+		 * @param clientID
+		 * @param order_type
+		 * @return 类型订单列表
+		 */
+		public List<OrderVO> getOrderList(String clientID,OrderType order_type){
+			List<OrderVO> order=new ArrayList<OrderVO>();
+			return order;
+		}
+		/**
+		 *得到客户的信用记录
+		 * @param clientID
+		 * @return 信用记录
+		 */
+		public File getCreditRecord(String clientID){
+			if(clientID!=null)
+				return new File(clientID);
+			else
+				return null;
+		}
+		/**
+		 *得到客户的历史预订酒店列表
+		 * @param clientID
+		 * @return 历史预订酒店列表
+		 */
+		public List<HotelinfoVO> getHistoryList(String clientID){
+			List<HotelinfoVO> hotel=new ArrayList<HotelinfoVO>();
+			return hotel;
+		}
+
 }
