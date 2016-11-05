12037b51a7c33e7432101fd37d3c8be8c5b10902
belikout <1243149017@qq.com>
2016-11-05 18:41:14 +0800

    wanyiwan～

---
+++ HRMSClient/src/test/java/ControllerTest/JustTest.java
@@ -0,0 +1,14 @@
+package ControllerTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JustTest {
+
+	@Test
+	public void test() {
+		assertEquals("ok","ok");
+	}
+
+}
