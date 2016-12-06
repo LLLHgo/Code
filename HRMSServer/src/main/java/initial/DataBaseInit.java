44c000bf5667c45fcc53390ae9009d0631a7112c
WinwindH <huangpiao@114-212-42-171.nju.edu.cn>
2016-12-06 19:55:48 +0800

    多传了一个。。

--- HRMSServer/src/main/java/initial/DataBaseInit.java
+++ HRMSServer/src/main/java/initial/DataBaseInit.java
@@ -23,34 +23,9 @@ public class DataBaseInit {
 		try{
 			// 注册 JDBC 驱动
 			Class.forName(JDBC_DRIVER);
-
-			// 打开链接
+    		// 打开链接
 			System.out.println("连接数据库...");
 			conn = DriverManager.getConnection(DB_URL,USER,PASS);
-			// 执行查询
-			System.out.println(" 实例化Statement对...");
-			stmt = conn.createStatement();
-			String sql;
-			sql = "SELECT MarketingID, name, tel FROM MarketingInfo";
-			ResultSet rs = stmt.executeQuery(sql);
-
-			// 展开结果集数据库
-			while(rs.next()){
-				// 通过字段检索
-				String id  = rs.getString("MarketingID");
-				String name = rs.getString("name");
-				String tel = rs.getString("tel");
-
-				// 输出数据
-				System.out.print("ID: " + id);
-				System.out.print(", 姓名: " + name);
-				System.out.print(", 电话: " + tel);
-				System.out.print("\n");
-			}
-			// 完成后关闭
-			rs.close();
-			stmt.close();
-			conn.close();
 		}catch(SQLException se){
 			// 处理 JDBC 错误
 			se.printStackTrace();
