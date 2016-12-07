fffc521f42267d13def98bd7658cbc16fc2bdb5a
WinwindH <huangpiao@huangpiaodeMacBook-Pro.local>
2016-12-07 22:17:15 +0800

    1

--- HRMSClient/src/test/java/bltest/marketingtest/InitTester.java
+++ HRMSClient/src/test/java/bltest/marketingtest/InitTester.java
@@ -12,11 +12,12 @@ public class InitTester {
 
 	@Test
 	public void test1(){
-		MarketingVO vo=new MarketingVO("网站营销人员","password","M00000001","13477778888");
-		assertEquals(vo.getName(),manage.init("M00000001").getName());
-		assertEquals(vo.getPassword(),manage.init("M00000001").getPassword());
-		assertEquals(vo.getMarketingID(),manage.init("M00000001").getMarketingID());
-		assertEquals(vo.getTelephone(),manage.init("M00000001").getTelephone());
+		MarketingVO vo=new MarketingVO("melody","password","M00000001","43748738743");
+		MarketingVO voFromBase=manage.init("M00000001");
+		assertEquals(vo.getName(),voFromBase.getName());
+		assertEquals(vo.getPassword(),voFromBase.getPassword());
+		assertEquals(vo.getMarketingID(),voFromBase.getMarketingID());
+		assertEquals(vo.getTelephone(),voFromBase.getTelephone());
 	}
 
 	@Test
@@ -24,4 +25,38 @@ public class InitTester {
 		assertEquals(null,manage.init("C000000001"));
 	}
 
+	@Test
+	public void test3(){//传入了不存在的账号
+		assertEquals(null,manage.init("M000000009"));
+	}
+
+	@Test
+	public void test4(){
+		MarketingVO vo=new MarketingVO("john","johnpass","M00000002","4343");
+		MarketingVO voFromBase=manage.init("M00000002");
+		assertEquals(vo.getName(),voFromBase.getName());
+		assertEquals(vo.getPassword(),voFromBase.getPassword());
+		assertEquals(vo.getMarketingID(),voFromBase.getMarketingID());
+		assertEquals(vo.getTelephone(),voFromBase.getTelephone());
+	}
+	
+	@Test
+	public void test5(){
+		MarketingVO vo=new MarketingVO("beauty","yes","M00000004","32");
+		MarketingVO voFromBase=manage.init("M00000004");
+		assertEquals(vo.getName(),voFromBase.getName());
+		assertEquals(vo.getPassword(),voFromBase.getPassword());
+		assertEquals(vo.getMarketingID(),voFromBase.getMarketingID());
+		assertEquals(vo.getTelephone(),voFromBase.getTelephone());
+	}
+	
+	@Test
+	public void test6(){
+		MarketingVO vo=new MarketingVO("handsome","that'sright","M00000008","387");
+		MarketingVO voFromBase=manage.init("M00000008");
+		assertEquals(vo.getName(),voFromBase.getName());
+		assertEquals(vo.getPassword(),voFromBase.getPassword());
+		assertEquals(vo.getMarketingID(),voFromBase.getMarketingID());
+		assertEquals(vo.getTelephone(),voFromBase.getTelephone());
+	}
 }
