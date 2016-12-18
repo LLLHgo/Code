509e176b7df2a80d105e16b83d63fff44b57cce3
sweetStreet <942290857@qq.com>
2016-12-18 16:34:01 +0800

    Revert "测试代码有毒啊啊啊"
    This reverts commit 7f3447713abc72a881a246fbbcecf20dbeb9be02.

--- HRMSServer/src/test/java/hotelstaff/FindTest.java
+++ HRMSServer/src/test/java/hotelstaff/FindTest.java
@@ -1,31 +1,34 @@
-package hotelstaff;
-
-import static org.junit.Assert.assertEquals;
-
-import java.rmi.RemoteException;
-
-import org.junit.Test;
-
-import dataservice.hotelstaffdataservice.HotelstaffDataService;
-import dataservice.hotelstaffdataservice.HotelstaffDataService_Stub;
-import impl.mysql.HotelstaffDataServiceMySqlImpl;
-
-public class FindTest {
-	HotelstaffDataService hotelstaff;
-	
-	public FindTest(){
-		hotelstaff = new HotelstaffDataService_Stub();
-	}
-	
-	@Test
-	public void test1() throws RemoteException{
-		//正确查找
-		assertEquals("000000",hotelstaff.find("H00000001").getPassword());
-	}
-	
-	@Test
-	public void test2() throws RemoteException{
-		//没有找到
-		assertEquals(null,hotelstaff.find("H00000002"));
-	}
-}
+//package hotelstaff;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//import java.rmi.RemoteException;
+//
+//import org.junit.Test;
+//
+//import dataservice.hotelstaffdataservice.HotelstaffDataService;
+//import impl.mysql.HotelstaffDataServiceMySqlImpl;
+//
+//public class FindTest {
+//	HotelstaffDataServiceMySqlImpl hotelstaff;
+//	
+//	public FindTest(){
+//		try {
+//			hotelstaff = new HotelstaffDataServiceMySqlImpl();
+//		} catch (RemoteException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	@Test
+//	public void test1() throws RemoteException{
+//		//正确查找
+//		assertEquals("000000",hotelstaff.find("H00000001").getPassword());
+//	}
+//	
+//	@Test
+//	public void test2() throws RemoteException{
+//		//没有找到
+//		assertEquals(null,hotelstaff.find("H00000002"));
+//	}
+//}
